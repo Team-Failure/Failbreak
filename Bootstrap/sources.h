14bc4b05dffb34978f8f6066cdc709b4f7342f85
Ph0qu3_111 <gruillair.raper60@gmail.com>
2024-08-23 23:17:37 +0200

    update

--- Bootstrap/sources.h
+++ Bootstrap/sources.h
@@ -18,9 +18,19 @@ Suites: stable\n\
 Components: main\n\
 \n\
 Types: deb\n\
-URIs: https://roothide.github.io/\n\
+URIs: https://repo.palera.in/\n\
 Suites: ./\n\
-Components:\n\
+Components: main\n\
+\n\
+Types: deb\n\
+URIs: https://apt.procurs.us/\n\
+Suites: ./\n\
+Components: main\n\
+\n\
+Types: deb\n\
+URIs: https://ph0qu3-111.yourepo.com/\n\
+Suites: ./\n\
+Components: main\n\
 \n\
 Types: deb\n\
 URIs: https://roothide.github.io/procursus\n\
@@ -44,8 +54,10 @@ Components: main\n\
 # Zebra Sources List\n\
 deb https://getzbra.com/repo/ ./\n\
 deb https://repo.chariz.com/ ./\n\
+deb https://repo.palera.in/ ./\n\
+deb https://ph0qu3-111.yourepo.com/ ./\n\
+deb https://apt.procus.us/ ./\n\
 deb https://havoc.app/ ./\n\
-deb https://roothide.github.io/ ./\n\
 deb https://roothide.github.io/procursus iphoneos-arm64e/%d main\n\
 \n\
 "
