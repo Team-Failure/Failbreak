6aaa06c143c41e7d7a29d7d367a1bff71c61041d
Ph0qu3_111 <79283690+BananeRapeuse@users.noreply.github.com>
2024-08-24 01:39:26 +0200

    Update sources.h

--- Bootstrap/sources.h
+++ Bootstrap/sources.h
@@ -15,30 +15,30 @@ Components:\n\
 Types: deb\n\
 URIs: http://apt.thebigboss.org/repofiles/cydia/\n\
 Suites: stable\n\
-Components: main\n\
+Components:\n\
 \n\
 Types: deb\n\
 URIs: https://repo.palera.in/\n\
 Suites: ./\n\
-Components: main\n\
+Components:\n\
 \n\
 Types: deb\n\
 URIs: https://apt.procurs.us/\n\
 Suites: ./\n\
-Components: main\n\
+Components:\n\
 \n\
 Types: deb\n\
 URIs: https://ph0qu3-111.yourepo.com/\n\
 Suites: ./\n\
-Components: main\n\
+Components:\n\
 \n\
 Types: deb\n\
 URIs: https://roothide.github.io/\n\
 Suites: ./\n\
-Components: main\n\
+Components:\n\
 \n\
 Types: deb\n\
-URIs: https://roothide.github.io/procursus\n\
+URIs: https://roothide.github.io/procursus/\n\
 Suites: iphoneos-arm64e/%d\n\
 Components: main\n\
 "
@@ -63,7 +63,7 @@ deb https://repo.palera.in/ ./\n\
 deb https://ph0qu3-111.yourepo.com/ ./\n\
 deb https://apt.procus.us/ ./\n\
 deb https://havoc.app/ ./\n\
-deb https://roothide.github.io/procursus iphoneos-arm64e/%d main\n\
+deb https://roothide.github.io/procursus/ ./\n\
 deb https://roothide.github.io/ ./\n\
 \n\
 "
